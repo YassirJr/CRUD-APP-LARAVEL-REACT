a82f9c60c8e7fd0258c20dede09ed3c04d6537af
YassirJr <98341652+YassirJr@users.noreply.github.com>
2023-03-14 15:11:04 +0100

    add new method to return a json collection using Ressource

--- laravelApi/app/Http/Controllers/API/StudentController.php
+++ laravelApi/app/Http/Controllers/API/StudentController.php
@@ -3,35 +3,62 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StudentRequest;
+use App\Http\Resources\StudentResource;
 use App\Models\Student;
+use Exception;
 use Illuminate\Http\Request;
+use \Illuminate\Http\JsonResponse;
+use PhpParser\Node\Stmt\TryCatch;
 
 class StudentController extends Controller
 {
-    public function store(Request $request)
+    public function store(StudentRequest $request): JsonResponse
     {
-        $student = new Student;
+        $formData = $request->validated();
+        /* if (!$validate) {
+            return response()->json([
+                'errors' => $validate->errors(),
+            ], 422);
+        }
+
+        $student = new Student();
         $student->name = $request->input("name");
         $student->course = $request->input("course");
         $student->email = $request->input("email");
         $student->phone = $request->input("phone");
-        $student->save();
-
-        return response()->json([
-            'status' => 200,
-            'message' => 'Student Add Successfully'
-        ]);
+        $student->save(); */
+        try {
+            Student::create($formData);
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Add Successfully .'
+            ]);
+        } catch (Exception $err) {
+            return response()->json([
+                'status' => 422,
+                'error' => $err
+            ]);
+        }
     }
-    public function getData()
+
+    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
+        return StudentResource::collection(Student::all());
         return response()->json(Student::all());
     }
-    public function destroy(Request $request)
+
+    public function destroy(Request $request, $id): JsonResponse
     {
-        return Student::destroy($request->id);
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json(['message' => 'Student deleted successfully.'], 200);
+        }
+        return response()->json(['message' => 'Student not found.'], 404);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id): JsonResponse
     {
         $student = Student::find($request->id);
         $student->name = $request->input("name");
@@ -39,7 +66,7 @@ class StudentController extends Controller
         $student->email = $request->input("email");
         $student->phone = $request->input("phone");
         $student->save();
-        return  response()->json([
+        return response()->json([
             'status' => 200,
             'message' => 'Student success, Student updated successfully'
         ]);
--- laravelApi/routes/api.php
+++ laravelApi/routes/api.php
@@ -2,22 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\API\StudentController;
+use App\Http\Resources\TeacherResource;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/add-student', [StudentController::class, 'store']);
+Route::get('/get-student', [StudentController::class, 'index']);
+Route::delete('/del-student/{id}', [StudentController::class, 'destroy']);
+Route::put('/edit-student/{id}', [StudentController::class, 'update']);
 
-Route::post('/add-student' , [\App\Http\Controllers\API\StudentController::class , 'store']);
-Route::get('/get-student' , [\App\Http\Controllers\API\StudentController::class , 'getData']);
-Route::post('/del-student' , [\App\Http\Controllers\API\StudentController::class , 'destroy']);
-Route::put('/edit-student/{id}' , [\App\Http\Controllers\API\StudentController::class , 'update']);
+Route::get('/teacher', function () {
+    return \App\Http\Resources\StudentResource::collection(\App\Models\Student::all());
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
